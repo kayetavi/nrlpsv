69ec1d6ff230e9b7c4758dd41a86a60b014f08a0
kayetavi <avijitkayet97@gmail.com>
2025-07-23 20:00:55 +0530

    Create psvData.js

---
+++ psvData.js
@@ -0,0 +1,7 @@
+const psvData = [
+  { psvNo: "PSV-1101", unit: "HCU", cdsp: "-", sp: "85 psi", bp: "-", orifice: '3/4"', type: "Open Bonnet" },
+  { psvNo: "TSV-1201", unit: "HCU", cdsp: "8 kg/cm²", sp: "100 psi", bp: "-", orifice: '1"', type: "Closed Bonnet" },
+  { psvNo: "PSV-2101", unit: "CDU", cdsp: "7 kg/cm²", sp: "90 psi", bp: "-", orifice: '1"', type: "Closed Bonnet" },
+  { psvNo: "TSV-2201", unit: "CDU", cdsp: "9 kg/cm²", sp: "110 psi", bp: "Yes", orifice: '1.5"', type: "Open Bonnet" },
+  { psvNo: "PSV-3101", unit: "CPP", cdsp: "10 kg/cm²", sp: "120 psi", bp: "Yes", orifice: '1.5"', type: "Pilot Operated" }
+];
