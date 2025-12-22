cdee2458d9cc84a2172868fff884a041f6f39829
kayetavi <avijitkayet97@gmail.com>
2025-12-22 15:40:15 +0530

    Add Firebase configuration file

---
+++ api/firebase-config.js
@@ -0,0 +1,9 @@
+// api/firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyBblnVz1SYbUGHfrAeFKte0-s4dRwrNLKM",
+  authDomain: "mysite-project-77071.firebaseapp.com",
+  projectId: "mysite-project-77071",
+  appId: "1:53846467224:web:171ebaa2ae19ab1a9166ed"
+};
+
+firebase.initializeApp(firebaseConfig);
